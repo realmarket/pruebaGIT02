fc4503717e70232398e0ec66fda14ccb7702f1f8
realmarket <jose@realmarket.es>
2014-05-29 19:34:16 +0200

    creado fichero index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+</head>
+
+<body>
+<h1>Bienvenidos pruebaGIT02</h1>
+<h2>Bienvenidos pruebaGIT02</h2>
+</body>
+</html>
